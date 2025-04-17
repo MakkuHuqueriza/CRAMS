587aaabb8753928d6fcaef5a329fbda2c74f3911
Jodi <jodi.gavano@gmail.com>
2025-04-18 04:10:59 +0800

    chore: initialize tailwind css

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: {
-    "postcss-import": {},
-    tailwindcss: {}, // If you're using Tailwind CSS
+    "@tailwindcss/postcss": {},
     autoprefixer: {},
   },
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 // tailwind.config.js
-module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+export const purge = [
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+];
+export const darkMode = false;
+export const theme = {
+  extend: {},
 };
+export const variants = {
+  extend: {},
+};
+export const plugins = [];
