5c5abb2298d16561ae7eb950b7d05d0e0e1c0029
MakkuHuqueriza <mhpena@up.edu.ph>
2025-04-19 14:22:11 +0800

    fix: build error

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -9,7 +9,6 @@ import {
   PopoverContent,
 } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
-import { logoutAction } from "@/actions/users"; // Import your logout action
 
 const Navbar = () => {
   return (
