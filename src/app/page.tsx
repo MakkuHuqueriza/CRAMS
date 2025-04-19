fbbd8ddd1f287279037b8072c950603386a7b4e1
Jodi <jodi.gavano@gmail.com>
2025-04-19 18:56:18 +0800

    feat: available rooms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Navbar from "@/app/components/Navbar";
 import AvailableRooms from "@/app/components/AvailableRooms";
+<<<<<<< HEAD
+=======
+import { getUser } from "@/utils/supabase/server"; // Import your getUser function
+import { redirect } from "next/navigation";
+>>>>>>> 10b80e4 (feat: available rooms)
 
 export default function HomePage() {
   return (
