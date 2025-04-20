76affefee79fee2335795cf2fe4e93f1582ef290
Jodi <jodi.gavano@gmail.com>
2025-04-21 06:25:34 +0800

    fix: remove unused variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Navbar from "@/app/components/Navbar";
 import AvailableRooms from "@/app/components/AvailableRooms";
-import { getUser } from "@/utils/supabase/server"; // Import your getUser function
-import { redirect } from "next/navigation";
 
 export default async function HomePage() {
   return (
