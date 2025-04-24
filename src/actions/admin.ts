1b6c20a51c5cfaf62d6898d572b1ce58c2032efb
Mark Jaily <mhpena@up.edu.ph>
2025-04-24 19:19:32 +0800

    feat: add admin authentication functionality
    * feat: add admin authentication functionality
    
    * refactor: refactor the admin auth logic  by using public schema
    
    * chore: npm run format

---
+++ src/actions/admin.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export const adminLoginAction = async (FormData: FormData) => {
+  const loginCredentials = {
+    email: FormData.get("email") as string,
+    password: FormData.get("password") as string,
+  };
+
+  const supabase = await createClient();
+
+  const { data, error } =
+    await supabase.auth.signInWithPassword(loginCredentials);
+
+  if (error) {
+    return error;
+  }
+
+  const { data: adminData, error: adminError } = await supabase
+    .from("admin")
+    .select("id")
+    .eq("id", data.user?.id)
+    .single();
+
+  if (adminError) {
+    return adminError;
+  }
+
+  if (adminData) {
+    redirect("/admin");
+  } else {
+    await supabase.auth.signOut();
+    redirect("/admin/login");
+  }
+};
+
+export const adminLogoutAction = async () => {
+  const { auth } = await createClient();
+
+  const { error } = await auth.signOut();
+
+  if (error) {
+    return error;
+  }
+
+  redirect("/admin/login");
+};
