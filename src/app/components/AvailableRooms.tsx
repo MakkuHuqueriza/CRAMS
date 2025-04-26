ea9f4a4e6877ff706510b91895f13447840b501e
Jodi <jodi.gavano@gmail.com>
2025-04-27 02:06:39 +0800

    feat: link to reserve

--- src/app/components/AvailableRooms.tsx
+++ src/app/components/AvailableRooms.tsx
@@ -166,7 +166,9 @@ const AvailableRooms = ({ roomDetails, roomTimes }: AvailableRoomsProps) => {
               md:text-[12px] md:px-3 md:py-2 
               text-[11px] px-3 py-2"
             >
+              <Link href={`/reserve`}>
               Reserve Now
+              </Link>
             </Button>
           </div>
         </div>
