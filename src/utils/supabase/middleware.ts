063fa8ce0dc1f684a0e7dff13384bc14f66e5d53
MakkuHuqueriza <mhpena@up.edu.ph>
2025-04-28 21:03:47 +0800

    chore: npm run format

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -54,7 +54,7 @@ export async function updateSession(request: NextRequest) {
   if (
     user &&
     (request.nextUrl.pathname.startsWith("/login") ||
-    request.nextUrl.pathname.startsWith("/signup"))
+      request.nextUrl.pathname.startsWith("/signup"))
   ) {
     // user is logged in, potentially respond by redirecting the user to the home page
     const url = request.nextUrl.clone();
