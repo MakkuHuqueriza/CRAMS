e3fcf566befd03fc7172912c8302134d55d664d6
MakkuHuqueriza <mhpena@up.edu.ph>
2025-04-29 20:33:10 +0800

    feat: add dynamic user profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import Navbar from "@/app/components/Navbar";
 import AvailableRooms from "@/app/components/AvailableRooms";
+import { getUser } from "@/utils/supabase/server";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const user = await getUser();
+
+  const name = user?.user_metadata.full_name;
+  const avatar_url = user?.user_metadata.avatar_url;
+  const email = user?.user_metadata.email;
+
   return (
     <main>
-      <Navbar />
+      <Navbar name={name} avatar_url={avatar_url} email={email} />
       <AvailableRooms />
     </main>
   );
