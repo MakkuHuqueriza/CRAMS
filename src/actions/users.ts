bef878032c3716186b0537f0236719cf1fa2a606
MakkuHuqueriza <mhpena@up.edu.ph>
2025-05-01 15:35:02 +0800

    chore: add getAllRoom function

--- src/actions/users.ts
+++ src/actions/users.ts
@@ -87,3 +87,19 @@ export const updatePasswordAction = async (FormData: FormData) => {
     return handleError(error);
   }
 };
+
+export const getAllRooms = async () => {
+
+  const supabase = await createClient();
+
+  const { data, error } = await supabase
+    .from("room")
+    .select("*")
+    .order("name", { ascending: true });
+  
+  if (error) {
+    return handleError(error);
+  }
+  return data;
+
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/app/components/Navbar";
 import AvailableRooms from "@/app/components/AvailableRooms";
 import Hero from "@/app/components/Hero";
 import { getUser } from "@/utils/supabase/server";
+import { getAllRooms } from "@/actions/users";
 
 export default async function HomePage() {
   const user = await getUser();
@@ -9,6 +10,10 @@ export default async function HomePage() {
   const name = user?.user_metadata.full_name;
   const avatar_url = user?.user_metadata.avatar_url;
   const email = user?.user_metadata.email;
+
+  const data = await getAllRooms();
+  console.log(data);
+
 
   return (
     <main>
