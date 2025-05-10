0bc29f02d0ce589bf6effba27e9a73dab060b8ee
MakkuHuqueriza <mhpena@up.edu.ph>
2025-05-11 00:28:41 +0800

    fix: add not admin indicator and comment out middleware

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -75,17 +75,17 @@ export async function updateSession(request: NextRequest) {
     return NextResponse.redirect(url);
   }
 
-  if (
-    user &&
-    isAdmin &&
-    (request.nextUrl.pathname === "/" ||
-      request.nextUrl.pathname === "/admin/login")
-  ) {
-    // user is logged in, potentially respond by redirecting the user to the admin page
-    const url = request.nextUrl.clone();
-    url.pathname = "/admin";
-    return NextResponse.redirect(url);
-  }
+  // if (
+  //   user &&
+  //   isAdmin &&
+  //   (request.nextUrl.pathname === "/" ||
+  //     request.nextUrl.pathname === "/admin/login")
+  // ) {
+  //   // user is logged in, potentially respond by redirecting the user to the admin page
+  //   const url = request.nextUrl.clone();
+  //   url.pathname = "/admin";
+  //   return NextResponse.redirect(url);
+  // }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
   // creating a new response object with NextResponse.next() make sure to:
