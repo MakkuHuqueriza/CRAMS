2f9efd6419b7530bbddccc09a544a776c3605ea8
Jodi <jodi.gavano@gmail.com>
2025-05-23 22:00:12 +0800

    feat: design details and linking

--- src/app/components/AvailableRooms.tsx
+++ src/app/components/AvailableRooms.tsx
@@ -166,7 +166,9 @@ const AvailableRooms = ({ roomDetails, roomTimes }: AvailableRoomsProps) => {
               md:text-[12px] md:px-3 md:py-2 
               text-[11px] px-3 py-2"
             >
-              Reserve Now
+              <Link href={`/rooms/${encodeURIComponent(room.name)}/reserve/page1`}>
+                Reserve Now
+              </Link>
             </Button>
           </div>
         </div>
--- src/app/components/RoomDetails.tsx
+++ src/app/components/RoomDetails.tsx
@@ -2,6 +2,7 @@
 
 import { useParams } from "next/navigation";
 import Image from "next/image";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import {
   Clock,
@@ -191,7 +192,9 @@ const RoomDetails = ({ roomDetails, roomTimes }: RoomsProps) => {
           {/* Reserve Button */}
           <div className="flex justify-start">
             <Button className="bg-[#274c77] text-white text-[14px] hover:bg-[#182657] px-5 py-6 rounded-[30px]">
-              Reserve Room
+              <Link href={`/rooms/${encodeURIComponent(room.name)}/reserve/page1`}>
+                Reserve Now
+              </Link>
             </Button>
           </div>
         </div>
